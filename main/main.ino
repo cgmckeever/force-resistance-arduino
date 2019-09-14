d56e02af45c3433189ed2727754220a807c8874b
cgmckeever <cgmckeever@gmail.com>
2019-09-14 11:44:35 -0500

    code clean

--- main/main.ino
+++ main/main.ino
@@ -10,16 +10,21 @@ Adafruit_SSD1306 display = Adafruit_SSD1306(128, 32, &Wire);
 #define BUTTON_B 16
 #define BUTTON_C  2
 
- 
-int fsrPin = A0;     // the FSR and 10K pulldown are connected to a0
+// the FSR and pulldown resistor are connected to a0 
+int fsrPin = A0;     
 int fsr0 = 0;
 int fsr1 = 0;
 int fsr2 = 0;
 float fsrMA = 0.0;
 
+// max voltage (based on pulldown resistor) seen on 
+// analog (A0) port, used to normlaize display
+int maxV = 1024;  
 int fsrN = 0;
-int maxV = 1024;
+
 int awake = 0;
+// shutdown after this long
+int shutdown_seconds; 
  
 void setup(void) {
   Serial.begin(9600); 
@@ -28,18 +33,10 @@ void setup(void) {
 
   // OLED 
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
- 
-  // Show image buffer on the display hardware.
-  // Since the buffer is intialized with an Adafruit splashscreen
-  // internally, this will display the splashscreen.
   display.display();
   delay(1000);
  
-  // Clear the buffer.
-  
- 
-  Serial.println("IO test");
- 
+  // set pbuttons for future use
   pinMode(BUTTON_A, INPUT_PULLUP);
   pinMode(BUTTON_B, INPUT_PULLUP);
   pinMode(BUTTON_C, INPUT_PULLUP);
@@ -47,6 +44,7 @@ void setup(void) {
 }
 
 void print(String reading){
+  // pad reading for display formating
   int len = 3 - reading.length();
   for(int i = 0; i < len; i++) {
     reading = ' ' + reading;  
@@ -70,22 +68,8 @@ void loop(void) {
  
   Serial.print("Analog reading = ");
   Serial.println(fsr0);  
-  
-  String message = "";
- 
-  // We'll have a few threshholds, qualitatively determined
-  if (fsr0 < 10) {
-    message = " - No pressure";
-  } else if (fsr0 < 200) {
-    message = " - Light touch";
-  } else if (fsr0 < 500) {
-    message = " - Light squeeze";
-  } else if (fsr0 < 800) {
-    message = " - Medium squeeze";
-  } else {
-    message = " - Big squeeze";
-  }
 
+  // calculate 3 point moving average, and normalize off max voltage
   fsrMA = ((float) fsr0 + (float) fsr1 + (float) fsr2) / 3;
   fsrN = round(fsrMA/ (float) maxV * 100);
   print((String) fsrN);
@@ -93,10 +77,10 @@ void loop(void) {
   delay(1000);
   awake = awake + 1;
 
-  if (awake > 5400) {
+  if (awake > shutdown_seconds) {
     display.clearDisplay();
     display.setCursor(7,0);
-    display.println("Goodbye");
+    display.println("Goodbye...");
     display.display();
     delay(3000);
     display.ssd1306_command(SSD1306_DISPLAYOFF);
